20a962f379647cc96d9bad21ea49ad67f1b22f22
Gyro <gamingskullgamer@gmail.com>
2022-12-07 14:38:56 +0100

    first commit

--- src/main/java/org/gyro/awsimageupload/config/AmazonConfig.java
+++ src/main/java/org/gyro/awsimageupload/config/AmazonConfig.java
@@ -6,17 +6,22 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
 
+    @Value("${aws-credentials-properties.accessKey}")
+    private String accessKey;
+    @Value("${aws-credentials-properties.secretKey}")
+    private String secretKey;
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "AKIA3TLFDH3SPZ2Q2RFR",
-                "q3hlVTJgJKRs8wrX0fS5ruOYqkBdWajSqonM4bGx"
+                accessKey,
+                secretKey
         );
 
         return AmazonS3ClientBuilder
